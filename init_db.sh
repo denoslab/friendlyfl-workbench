14f4a5a8ffaf310738b999fa37a12f0594a9f468
Meng Zhai <meng.zhai@cyberx.com>
2023-08-11 00:24:32 -0400

    FL-40 Support Celery Workers

--- init_db.sh
+++ init_db.sh
@@ -1,4 +1,4 @@
-sudo docker exec -i $(sudo docker ps | grep "postgres_1\|postgres-1" | cut -f1 -d" ") \
-psql -Upostgres -p 5432 << EOF
-create database friendlyfl-router;
+docker exec -i $(sudo docker ps | grep "postgres\|postgres" | cut -f1 -d" ") \
+psql -U postgres << EOF
+CREATE DATABASE "friendlyfl-router";
 EOF
